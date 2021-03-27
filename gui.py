0f815fdb9e29f245e8acf5f03eab1f9127b60804
qkrtndh <qkrtndh5@naver.com>
2021-03-27 16:41:36 +0900

    타이틀 수정, 로딩이미지 or gif추가, 로딩  게이지 추가, 이미지 포멧 플러그인 추가, 파일선택 필터  추가

--- gui.py
+++ gui.py
@@ -1,48 +1,75 @@
 import sys
+from PyQt5 import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtGui import *
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import *
+import math
+
 import os
 import cv2
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
+
 form_class = uic.loadUiType("test.ui")[0]
 
 #화면을 띄우는데 사용되는 Class 선언
 class WindowClass(QMainWindow, form_class) :
+    app = QApplication(sys.argv)
+    app.addLibraryPath(r"C:\\Users\\박수오\\AppData\\Local\\Programs\\Python\\Python38\\Lib\\site-packages\\PyQt5\\Qt\\plugins")
     def __init__(self) :
+        
         super().__init__()
         self.setupUi(self)
-
+        
+        self.setWindowTitle("알려줘! 카즈")
         self.qPixmapVar = QPixmap()
         self.qPixmapVar.load("carz.png")
         self.qPixmapVar = self.qPixmapVar.scaledToWidth(600)
         self.imageview.setPixmap(self.qPixmapVar)
+        self.progressBar.setValue(0)
+        self.progressBar.hide()
         
         self.btn_upload.clicked.connect(self.loadImageFromFile)
 
     def loadImageFromFile(self):
-        filename = QtWidgets.QFileDialog.getOpenFileName(self,'Open File')
+        filename = QtWidgets.QFileDialog.getOpenFileName(self,'Open File','','Image File(*.jpg *png);; Video File(*.avi *mp4)')
         fn,fe=os.path.splitext(filename[0])
         if fe[1:4] == 'png' or fe[1:4] == 'jpg':
             self.qPixmapVar.load(filename[0])
             self.qPixmapVar = self.qPixmapVar.scaledToWidth(600)
             self.imageview.setPixmap(self.qPixmapVar)
         elif fe[1:4] == 'mp4' or fe[1:4] == 'avi':
+            self.progressBar.setVisible(True)
             vidcap = cv2.VideoCapture(filename[0])
             success,cutimage = vidcap.read()
             cnt = 1
             success=True
+            """self.qPixmapVar.load("wait0.png")
+            self.qPixmapVar = self.qPixmapVar.scaledToWidth(600)
+            self.imageview.setPixmap(self.qPixmapVar)"""
+
+            self.loding_img=QMovie('loding.gif',QByteArray(),self)
+            self.loding_img.setCacheMode(QMovie.CacheAll)
+            self.imageview.setMovie(self.loding_img)
+            self.loding_img.start()
+
+
+            vidcount = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))//20
             while success:
                 success,cutimage = vidcap.read()
                 if(int(vidcap.get(1))%20==0):
+                    self.progressBar.setValue((cnt/vidcount)*100)
                     cv2.imwrite("D:\cuted_image\%d.jpg" % cnt,cutimage)
                     print("saved image %d.jpg" % cnt)
                     cnt+=1
-                if cv2.waitKey(10) == 5:
+                    """self.qPixmapVar.load("wait"+str(cnt%3)+".png")
+                    self.qPixmapVar = self.qPixmapVar.scaledToWidth(600)
+                    self.imageview.setPixmap(self.qPixmapVar)"""
+                if cv2.waitKey(10) == 27:
                     break
-                
+             
         print(filename[0])
 
 if __name__ == "__main__" :
