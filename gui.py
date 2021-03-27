2f9bd6cd1e52ff369635dbc254f75eec14a6fe70
qkrtndh <qkrtndh5@naver.com>
2021-03-27 18:03:25 +0900

    경고창 추가 및 이미지저장경로 수정, 무시등록, 테스트 ui추가

--- gui.py
+++ gui.py
@@ -18,20 +18,26 @@ form_class = uic.loadUiType("test.ui")[0]
 class WindowClass(QMainWindow, form_class) :
     app = QApplication(sys.argv)
     app.addLibraryPath(r"C:\\Users\\박수오\\AppData\\Local\\Programs\\Python\\Python38\\Lib\\site-packages\\PyQt5\\Qt\\plugins")
+    
     def __init__(self) :
         
         super().__init__()
         self.setupUi(self)
         
+        self.ch = 0
+
         self.setWindowTitle("알려줘! 카즈")
         self.qPixmapVar = QPixmap()
         self.qPixmapVar.load("carz.png")
         self.qPixmapVar = self.qPixmapVar.scaledToWidth(600)
+
         self.imageview.setPixmap(self.qPixmapVar)
+
         self.progressBar.setValue(0)
         self.progressBar.hide()
         
         self.btn_upload.clicked.connect(self.loadImageFromFile)
+        self.btn_ok.clicked.connect(self.doOperation)
 
     def loadImageFromFile(self):
         filename = QtWidgets.QFileDialog.getOpenFileName(self,'Open File','','Image File(*.jpg *png);; Video File(*.avi *mp4)')
@@ -61,7 +67,7 @@ class WindowClass(QMainWindow, form_class) :
                 success,cutimage = vidcap.read()
                 if(int(vidcap.get(1))%20==0):
                     self.progressBar.setValue((cnt/vidcount)*100)
-                    cv2.imwrite("D:\cuted_image\%d.jpg" % cnt,cutimage)
+                    cv2.imwrite("cuted_img\%d.jpg" % cnt,cutimage)
                     print("saved image %d.jpg" % cnt)
                     cnt+=1
                     """self.qPixmapVar.load("wait"+str(cnt%3)+".png")
@@ -69,8 +75,16 @@ class WindowClass(QMainWindow, form_class) :
                     self.imageview.setPixmap(self.qPixmapVar)"""
                 if cv2.waitKey(10) == 27:
                     break
-             
+        self.ch=1
         print(filename[0])
+        
+    def doOperation(self):
+        if self.ch == 0:
+            print("choose file")
+            buttonReply = QMessageBox.question(self, '경고!', "이미지가 선택되지 않았습니다!", QMessageBox.Yes)
+        
+
+
 
 if __name__ == "__main__" :
     #QApplication : 프로그램을 실행시켜주는 클래스
